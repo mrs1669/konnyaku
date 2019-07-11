deed9afe77b2e78fc71d6c7e7f6e3d4b5a1de1ac
Takumi Muraishi <mrs1669.develop@gmail.com>
2019-07-11 15:28:33 +0900

    テーブルビュー表示

--- studyTableView/studyTableView/ViewController.swift
+++ studyTableView/studyTableView/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // tableview instance
+    private var tableView: UITableView!
+    
+    // Array to display tableview
+    private var items: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        items = ["Apple", "Water melon", "Peach", "Cherry", "Grapes", "pear"]
+        
+        // initialize tableview
+        tableView = UITableView()
+        
+        // set delegate
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // set tableview size
+        tableView.frame = view.frame
+        
+        // set tableview
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: NSStringFromClass(UITableViewCell.self))
+        view.addSubview(tableView)
     }
-
-
+    
+    // MARK: tableview delegate methods
+    // set the number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    // set tableview rows
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: NSStringFromClass(UITableViewCell.self))! as UITableViewCell
+        cell.textLabel?.text = self.items[indexPath.row]
+        return cell
+    }
+    
+    // called when tableview cell was tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("\(indexPath.row) cell was selected")
+    }
 }
 
